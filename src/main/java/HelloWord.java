e7d31354f89f5a18f57ac02e3e5c9c85995bbb29
FIT <FIT@EasyIT>
2014-12-21 09:08:19 +0700

    First Commit

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FIT
+ */
+public class HelloWord 
+{
+    public static void main(string[] args)
+    {
+        System.out.println("HelloWord");
+    }
+}
